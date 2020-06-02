4325a5ca84efcfcd4e9be7374c810afdc2546df5
duardopx <duardopx@ufrn.edu.br>
2020-06-02 00:18:33 -0300

    Stack finalization

--- data-structures/stack/stack.c
+++ data-structures/stack/stack.c
@@ -1,83 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <limits.h>
 
-/**
- * Pensar em alto nivel
- * estruturar o pensamento
- * implementar
- * */
 
-#define STACK_SIZE 0x64
-
-int stack[STACK_SIZE];
-
-int stack_pointer = -1;
-
-char stack_empty()
+struct stack
 {
-	if (stack_pointer < 0x0)
-	{
-		return 0x1;
-	}
+	int size;
+	int top;
+	int * array;
+	unsigned capacity;
+};
 
-	return 0x0;
+bool is_empty(struct stack * stack)
+{
+	return  stack->size == 0x0;
 }
 
-void push(int * stack, int value)
+bool is_full (struct stack * stack)
 {
+	return stack->size == stack->capacity;
+}
 
-	if (stack_pointer >= STACK_SIZE)
-	{
+struct stack * create_stack(unsigned capacity)
+{
+	struct stack * new_stack = (struct stack *)malloc(sizeof(struct stack));
+	new_stack->capacity = capacity;
+	new_stack->top = -0x1;
+	new_stack->size = 0x0;
+	new_stack->array = (int *)malloc(new_stack->capacity * sizeof(int));
+
+	return new_stack;
+}
+
+void push(struct stack * stack, int value)
+{
+	if (is_full(stack))
 		return;
-	}
 
-	stack_pointer += 0x1;
-	printf("> stack_pointer value: [%d]\n", stack_pointer);
-	stack[stack_pointer] = value;
+	stack->size++;
+	stack->array[++stack->top] = value;
 }
 
-int pop(int * stack)
+int pop(struct stack * stack)
 {
-	int return_element;
+	if (is_empty(stack))
+		return INT_MIN;
 
-	if (stack_empty() == 0x1)
-	{
-		puts("> Stack is empty");
-		return 0x0;
-	}
-
-	return_element = stack[stack_pointer];
-	stack_pointer -= 0x1;
-
-	return return_element;
+	stack->size--;
+	return stack->array[stack->top--];
 }
 
-void print_stack()
+
+void print_struct(struct stack * stack)
 {
 	int i;
 
-	for (i = 0x0; i <= stack_pointer; i++)
+	for (i = 0x0; i < stack->size; i++)
 	{
-		printf("{%d:[%d]}\n", i, stack[i]);
+		printf("{%d:[%d]}\n", i, stack->array[i]);
 	}
+
 	puts("");
 }
 
 
 int main(int argc, char ** argv)
 {
-	/* push(stack, 0xa); */
-	/* push(stack, 0xb); */
-	/* push(stack, 0xc); */
-	/* push(stack, 0xd); */
+	struct stack * stack = create_stack(0xa);
 
-	print_stack();
+	push(stack, 0xa);
+	push(stack, 0x1);
+	push(stack, 0x2);
+	push(stack, 0x3);
+	push(stack, 0x4);
+
+	print_struct(stack);
 
 	pop(stack);
 	pop(stack);
 
-	print_stack();
+	print_struct(stack);
+
+	push(stack, 0x3);
+	push(stack, 0x4);
+
+	print_struct(stack);
+
 
 	return 0x0;
 }
